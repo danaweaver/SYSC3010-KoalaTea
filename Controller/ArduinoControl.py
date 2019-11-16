17dc7fd1212044ee4e5a635fbea143033ea8cc41
vivianle1333 <vivian.le1333@hotmail.com>
2019-11-16 17:44:40 -0500

    Implemented ArduinoControl (code has not been tested)

--- Controller/ArduinoControl.py
+++ Controller/ArduinoControl.py
@@ -1,27 +1,50 @@
 import json, socket, sys, time, serial
 
 class ArduinoControl:
+
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyACM0', 9600)
+        self.ser.open()
     
     def measureWater(self, temp):
-        return True
+        self.ser.write("tStart".encode('utf-8'))
+        while True:
+            data = ser.readline()
+            measTemp = float(data.split("T")[1])
+            if measTemp >= temp:
+                break
+        self.stopTemperature()
+    
+    def stopTemperature(self):
+        self.ser.write("tStop".encode('utf-8'))
     
     def lowerTeaBag(self):
-        return True
+        self.sendReceive("50", "lowerTea")
 
     def raiseTeaBag(self):
-        return True
+        self.sendReceive("51", "raiseTea")
 
     def displayTimer(self, time):
-        return True
+        self.sendReceive("6" + str(time), "6Done")
     
     def stopTimer(self):
-        return True
-    
+        self.ser.write("6Stop".encode('utf-8'))
+
     def turnOnLED(self):
-        return True
+        self.ser.write("71".encode('utf-8'))
 
     def turnOffLED(self):
-        return True
+        self.ser.write("70".encode('utf-8'))
 
     def reset(self):
-        return True
+        self.stopTemperature()
+        self.raiseTeaBag()
+        self.stopTimer()
+        self.turnOffLED()
+
+    def sendReceive(self, send, response):
+        self.ser.write(send.encode('utf-8'))
+        while True:
+            data = ser.readline()
+            if(data == response):
+                break
